2224c5a0ee9f7bafc0321866598e47ba891f27ae
Gavin Brown <iamslowtrain@gmail.com>
2024-02-13 16:55:45 +0000

    add the final three html templates, order/sales/startscreen.

--- pointofsale/routes.py
+++ pointofsale/routes.py
@@ -10,3 +10,28 @@ def home():
 @app.route("/categories")
 def categories():
     return render_template("categories.html")
+
+
+@app.route("/subcategories")
+def subcategories():
+    return render_template("subcategories.html")
+
+
+@app.route("/items")
+def items():
+    return render_template("items.html")
+
+
+@app.route("/order")
+def order():
+    return render_template("order.html")
+
+
+@app.route("/sales")
+def sales():
+    return render_template("sales.html")
+
+
+@app.route("/startscreen")
+def startscreen():
+    return render_template("startscreen.html")
