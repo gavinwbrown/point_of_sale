75bf42b11fa72230578eeb00b8607c063f3e56b3
Gavin Brown <iamslowtrain@gmail.com>
2024-02-14 16:29:41 +0000

    add further html templates and provide basic navigation links between all current pages.

--- pointofsale/routes.py
+++ pointofsale/routes.py
@@ -4,7 +4,7 @@ from pointofsale import app, db
 
 @app.route("/")
 def home():
-    return render_template("base.html")
+    return render_template("startscreen.html")
 
 
 @app.route("/categories")
@@ -12,14 +12,29 @@ def categories():
     return render_template("categories.html")
 
 
+@app.route("/add_category", methods=["GET", "POST"])
+def add_category():
+    return render_template("add_category.html")
+
+
 @app.route("/subcategories")
 def subcategories():
     return render_template("subcategories.html")
 
 
+@app.route("/add_sub_category", methods=["GET", "POST"])
+def add_sub_category():
+    return render_template("add_sub_category.html")
+
+
 @app.route("/items")
 def items():
     return render_template("items.html")
+
+
+@app.route("/add_item", methods=["GET", "POST"])
+def add_item():
+    return render_template("add_item.html")
 
 
 @app.route("/order")
